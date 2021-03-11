f0654002066c0ec58b34a9f6a5ec6d25cb29ec1e
bitdeep <19378023+bitdeep@users.noreply.github.com>
2021-03-11 16:48:41 -0300

    change contracts to testnet

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,15 +1,15 @@
 export default {
   cake: {
     56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
-    97: '',
+    97: '0xdefA606701544521fFCa2Dc6D0E84f6A85995Dd4',
   },
   masterChef: {
     56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
-    97: '',
+    97: '0xfaE896751d5e75eB5B0f911ce5e987388837646f',
   },
   wbnb: {
     56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    97: '',
+    97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
   },
   lottery: {
     56: '',
@@ -25,6 +25,6 @@ export default {
   },
   busd: {
     56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-    97: '',
+    97: '0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee',
   },
 }

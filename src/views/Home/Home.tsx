58e454461e078c372e3f27d94d1dcfda0b93a022
shrijana <shrijana1991@gmail.com>
2021-03-14 16:12:46 +0545

    fix: update subtitle

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -62,7 +62,7 @@ const Home: React.FC = () => {
         <Heading as="h1" size="xl" mb="24px" color="secondary">
           {TranslateString(576, 'Vlad Finance')}
         </Heading>
-        <Text>{TranslateString(578, 'Top 3 best DEFI app on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'Come for the Immortality. Stay for the NFTs.')}</Text>
       </Hero>
       <div>
         <Cards>
